a63dfb702ebdf138baabc877f46da92960012896
loginkaushal <loginkaushal@gmail.com>
2018-09-15 08:37:01 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+(function(angular) {
+	'use strict';
+
+
+var app = angular.module('SimpleInterest',[]);
+
+app.controller('main', function ($scope) {
+	
+	//$scope.Principle = Principle;
+	//$scope.ROI = ;
+	//$scope.Term = 20;
+	
+	$scope.myInterest = function (){
+	
+	$scope.interest = $scope.Principle * $scope.ROI * $scope.Term/100;
+	
+	console.log($scope.Principle);
+	console.log($scope.interest);
+	}
+});
+
+})(window.angular);
